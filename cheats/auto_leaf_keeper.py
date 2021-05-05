5e62b6f4e52ace2c890e4d4b94e2c944b58855ff
SClausen1 <42357672+SClausen1@users.noreply.github.com>
2021-05-04 23:00:37 -0400

    changed file structure

---
+++ cheats/auto_leaf_keeper.py
@@ -0,0 +1,24 @@
+import mouse 
+import threading 
+import keyboard
+import pickle
+
+
+    
+    
+
+def mouse_mover(record):
+    while True:
+        mouse.play(record)
+
+def main_loop():
+    with open("recordings/auto_leaf_keaper.pkl", 'r') as input:
+        record = pickle.load(input)
+    print('press enter to stop auto leaf keaper')
+    click_listener_thread = threading.Thread(target=keyboard.wait, args=('enter',))
+
+    click_listener_thread.start()
+
+    while click_listener_thread.is_alive():
+        mouse.play(record, speed_factor=10)
+
