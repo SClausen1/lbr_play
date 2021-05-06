2ef720d1d4e8c8a4f3640f3f6344141d7e6dfacd
SClausen1 <42357672+SClausen1@users.noreply.github.com>
2021-05-06 02:41:05 -0400

    cleaned imports, shrank moniter, small changes

--- cheats/blc_time_travel.py
+++ cheats/blc_time_travel.py
@@ -1,19 +1,11 @@
-
 import pytesseract
-
-
-
 import numpy as np
 import cv2
-from PIL import ImageGrab, Image
 import mss
-import os
-import os.path
-import time
-
-import requests
-from urllib.request import urlopen
+import threading
 from helpers import set_time
+import keyboard
+import time
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
@@ -29,56 +21,55 @@ def main_loop():
 def time_travel():
     with mss.mss() as sct:
         # Part of the screen to capture
-        monitor = {"top": 40, "left": 0, "width": 1500, "height": 800}
+        monitor = {"top": 205, "left": 290, "width": 1200, "height": 640}
 
-        while "Screen capturing":
-            
-
-            # Get raw pixels from the screen, save it to a Numpy array
-            print('Grabbing screen in {}' 5)
-            time.sleep(5)   
-            # invert image, to get black text
-            inverted_img = invert_screenshot(sct.grab(monitor))
-        
-            time_dict = extract_relevant_text(pytesseract.image_to_string(inverted_img))
-
-            seconds = dict_to_seconds
-            
-            set_time._win_set_time(seconds)
-            time.sleep(20)
+        # Get raw pixels from the screen, save it to a Numpy array
+        print('Grabbing screen in 5')
+        time.sleep(5)   
+        # invert image, to get black text
+        print('Inverting image')
+        inverted_img = invert_screenshot(sct.grab(monitor))
+        print('Extracting Text')
+        time_dict = extract_relevant_text(pytesseract.image_to_string(inverted_img))
+        if(time_dict == None):
+            print("time travel failed")
+            return
+        seconds = dict_to_seconds(time_dict)
+        print('moving forward {} seconds'.format(seconds))
+        set_time._win_set_time(seconds)
+        time.sleep(20)
   
 
-def dict_to_seconds(time_dict)
-    for key in time_dicts.keys:
-            if key == 'seconds':
-                time_remaining += int(time_dicts.get(key))
-            elif key =='hours':
-                time_remaining += int(time_dicts.get(key)) * 3600
-            elif key == 'minutes':
-                time_remaining += int(time_dicts.get(key)) * 60
-            else:
-                time_remaining += int(time_dicts.get(key)) * 3600 * 24
-
-def extract_relevant_text(lines)
-    lines = text.splitlines()
-    try:
-        index = lines.index("Orb of ELC fon")
-    except ValueError:
-        print(text)
-        break
-    
-    line = lines[index + 2].split
-    print(line)
-    time = line[line.index('in')+1:]
-    time_dict = {}
+def dict_to_seconds(time_dict):
     time_remaining = 0
+    if time_dict:
+        for key in time_dict.keys():
+                if key == 'seconds':
+                    time_remaining += time_dict.get(key)
+                elif key =='hours':
+                    time_remaining += time_dict.get(key) * 3600
+                elif key == 'minutes':
+                    time_remaining += time_dict.get(key) * 60
+                else:
+                    time_remaining += time_dict.get(key) * 3600 * 24
+    return time_remaining
 
-    for i in range(len(time), step=2):
-        time_dict[time[i+1]] = time[i]
-    return time_dict
+def extract_relevant_text(text):
+    lines = text.splitlines()
+    for line in lines:
+        if line.startswith('Orb of BLC'):
+            index = lines.index(line)  
+            time_line = lines[index + 3].split(' in')[2].strip('.').split()
+            time_dict = {}
+            for i in range(0,len(time_line),2):
+                time_dict[time_line[i+1]] = int(time_line[i])
+            return time_dict
+            
+    return None
+
 
 def invert_screenshot(screensthot):
-    img = np.array(screensthot))
+    img = np.array(screensthot)
     greyscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     inverted_img = cv2.bitwise_not(greyscale_img)
-    cv2.imshow("Image", inverted_img)
+    return inverted_img
