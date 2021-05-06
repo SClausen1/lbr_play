16506abba3c04f3b30664cc60ac54dd9ecc7ec95
SClausen1 <42357672+SClausen1@users.noreply.github.com>
2021-05-06 02:38:57 -0400

    trying SetLocalTime instead of SetSystemTime

--- helpers/set_time.py
+++ helpers/set_time.py
@@ -6,13 +6,14 @@ import time
 def _win_set_time(offset):
     import win32api
     time = calculate_time(offset)
-    win32api.SetSystemTime(*time)
+    win32api.SetLocalTime(time)
 
 def calculate_time(offset):
     time = datetime.now()
     td = timedelta(seconds=offset)
     wantedTime = time + td
-    dayOfWeek = wantedTime.isocalendar()[2]
-    wantedTup = wantedTime.timetuple()
-    return  wantedTup[:2] + (dayOfWeek,) + wantedTup[2:6] + (0,)
+    return wantedTime
+    # dayOfWeek = wantedTime.isocalendar()[2]
+    # wantedTup = wantedTime.timetuple()
+    # return  wantedTup[:2] + (dayOfWeek,) + wantedTup[2:6] + (0,)
     
