8e45a413a1f21bd5b988ccb223ad54244cffcb9f
SClausen1 <42357672+SClausen1@users.noreply.github.com>
2021-05-11 14:14:35 -0400

    using utc

--- helpers/set_time.py
+++ helpers/set_time.py
@@ -6,13 +6,22 @@ import time
 def _win_set_time(offset):
     import win32api
     time = calculate_time(offset)
-    win32api.SetLocalTime(time)
+    year = int(time.year)
+    month = int(time.month)
+    # Return the day of the week as an integer, where Monday is 0 and Sunday is 6.
+    dayOfWeek = int(time.weekday())
+    day = int(time.day)
+    hour = int(time.hour)
+    minute = int(time.minute)
+    second = int(time.second)
+    millseconds = int((time.microsecond)/1000)
+
+    win32api.SetSystemTime(year,month,dayOfWeek,day,hour,minute,second,millseconds)
 
 def calculate_time(offset):
-    time = datetime.now()
-    td = timedelta(seconds=offset)
-    wantedTime = time + td
-    return wantedTime
+    time = datetime.utcnow()
+    return time + timedelta(seconds=offset)
+ 
     # dayOfWeek = wantedTime.isocalendar()[2]
     # wantedTup = wantedTime.timetuple()
     # return  wantedTup[:2] + (dayOfWeek,) + wantedTup[2:6] + (0,)
